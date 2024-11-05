01e471837d420493221bf872d4afcc236f0be911
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-05 22:43:32 +0300

    Added role

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# Define a custom User model to handle different roles (admin, farmer, technician):
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('farmer', 'Farmer'),
+        ('technician', 'Technician')
+    ]
+    role = models.CharField(
+        max_length=10, choices=ROLE_CHOICES, default='farmer')
