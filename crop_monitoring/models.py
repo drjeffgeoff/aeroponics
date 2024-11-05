ec6162749b2c385f7923ba43612abaed7ab3660c
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-05 22:45:14 +0300

    Added crop models

--- crop_monitoring/models.py
+++ crop_monitoring/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from sensor.models import SensorData
 
-# Create your models here.
+
+class Crop(models.Model):
+    name = models.CharField(max_length=100)
+    sensor_data = models.ForeignKey(SensorData, on_delete=models.CASCADE)
+    growth_stage = models.CharField(max_length=100)
+    health_status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
