98a0f2cf246183fc72c853b9f9c79484c973913e
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-05 22:44:34 +0300

    Added Sensor models

--- sensor/models.py
+++ sensor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class SensorData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    pH = models.FloatField()
+    light_intensity = models.FloatField()
+    nutrient_level = models.FloatField()
+
+    def __str__(self):
+        return f"Data from {self.user} at {self.timestamp}"
