f38f957466885a3a95a522aaaf9a5e47dba3ef7d
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-05 22:59:57 +0300

    commited

--- sensor/views.py
+++ sensor/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import SensorData
+from .serializers import SensorDataSerializer
 
-# Create your views here.
+
+class SensorDataViewSet(viewsets.ModelViewSet):
+    queryset = SensorData.objects.all()
+    serializer_class = SensorDataSerializer
+    permission_classes = [IsAuthenticated]
