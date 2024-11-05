1e5359936f7671a2b57c9ed6eef40d5e437bba13
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-05 22:53:26 +0300

    Added models for recommendations

--- recommendation/models.py
+++ recommendation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class CropRecommendation(models.Model):
+    crop_name = models.CharField(max_length=100)
+    optimal_ph = models.FloatField()
+    optimal_temp = models.FloatField()
+    nutrient_requirement = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.crop_name
