ebf20f8a2a42e9958479b4da48a96eff220b6353
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-07 13:17:26 +0300

    Modified the url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
-# accounts/urls.py
-from django.urls import path
-from . import views
+from django.urls import path, include
+# Import the homepage view from the 'accounts' app
+from accounts.views import homepage
 
 urlpatterns = [
-    path('', views.index, name='accounts_index'),  # Example view
+    path('admin/', admin.site.urls),
+    path('api/sensor/', include('sensor.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/crop_monitoring/', include('crop_monitoring.urls')),
+    path('api/recommendation/', include('recommendation.urls')),
+    path('', homepage, name='homepage'),  # Root URL
 ]
