cb40e2c5924fb1396175422d375edc41362af7dc
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-07 13:13:03 +0300

    Commited

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Define a custom User model to handle different roles (admin, farmer, technician):
-
 
 class User(AbstractUser):
     ROLE_CHOICES = [
@@ -10,5 +8,20 @@ class User(AbstractUser):
         ('farmer', 'Farmer'),
         ('technician', 'Technician')
     ]
+
     role = models.CharField(
         max_length=10, choices=ROLE_CHOICES, default='farmer')
+
+    # Override groups and user_permissions fields with unique related names
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name="custom_user_groups",
+        blank=True,
+        help_text="The groups this user belongs to."
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name="custom_user_permissions",
+        blank=True,
+        help_text="Specific permissions for this user."
+    )
