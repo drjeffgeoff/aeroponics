470298c3daa4590c396ed651d5f8c39f345dce00
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-07 12:33:18 +0300

    Modiefied views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# accounts/views.py
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Welcome to the Accounts app!")
