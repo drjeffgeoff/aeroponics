709f1581efb3a7cf2895f754e370d216530004b9
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-11-07 12:26:14 +0300

    Commited

--- sensor/views.py
+++ sensor/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import SensorData
@@ -8,3 +9,10 @@ class SensorDataViewSet(viewsets.ModelViewSet):
     queryset = SensorData.objects.all()
     serializer_class = SensorDataSerializer
     permission_classes = [IsAuthenticated]
+
+
+# sensor/views.py
+
+
+def index(request):
+    return HttpResponse("Welcome to the Sensor app!")
